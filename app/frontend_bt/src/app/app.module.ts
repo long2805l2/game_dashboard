1293d787d4fea69504b03a642837fec5b4e6ef08
skyer <long2805l2@gmail.com>
2018-11-12 20:37:05 +0700

    -update sidebar, main

--- app/frontend_bt/src/app/app.module.ts
+++ app/frontend_bt/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { MainComponent } from './component/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    SidebarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
